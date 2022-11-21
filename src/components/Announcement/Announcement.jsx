fa1596d92658cf39b21ea31670d252aa236143fc
Yasser-Shehab <yassershehab9@gmail.com>
2022-11-21 20:28:46 +0200

    Added Announcment Component

--- src/components/Announcement/Announcement.jsx
+++ src/components/Announcement/Announcement.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import classes from "./Announcement.module.scss";
+import { announces } from "../../service/Announcment";
 
 const Announcement = () => {
-  return <div>Announcement</div>;
+  return (
+    <section className={classes.announceContainer}>
+      <div className={classes.heading}>
+        <div className={classes.title}>
+          <h1>Announcments</h1>
+          <p>We educate winners keep updated</p>
+        </div>
+        <div className={classes.expandAll}>All</div>
+      </div>
+      <div>
+        <ul className={classes.listContainer}>
+          {announces.map((item) => {
+            return (
+              <li key={item.id} className={classes.list}>
+                <div className={classes.info}>
+                  <img src={item.icon} alt="Profile Icon" />
+                  <div>
+                    <p>{item.name}</p>
+                    <p className={classes.spec}>{item.specialization}</p>
+                  </div>
+                </div>
+                <div>
+                  <p className={classes.description}>{item.description}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Announcement;
