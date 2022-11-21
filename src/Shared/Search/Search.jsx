39a77a19a2ac35e133e76a7c3e5dd3309a284e41
Yasser-Shehab <yassershehab9@gmail.com>
2022-11-21 18:27:41 +0200

    navbar

--- src/Shared/Search/Search.jsx
+++ src/Shared/Search/Search.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import classes from "./Search.module.scss";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className={classes.searchContainer}>
+      <div className={classes.searchIcon}>
+        <HiMagnifyingGlass />
+      </div>
+      <div>
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Search from "../../Shared/Search/Search";
 import classes from "./Navbar.module.scss";
+import { AiFillBell } from "react-icons/ai";
+import { HiMail } from "react-icons/hi";
+import user from "../../assets/images/AquaApple.png";
 
 const Navbar = () => {
   return (
@@ -8,8 +11,19 @@ const Navbar = () => {
       <div className={classes.message}>
         <h1>Welcome User,</h1>
       </div>
-      <div>
+      <div className={classes.accountUtils}>
         <Search />
+        <div className={classes.icons}>
+          <span>
+            <AiFillBell className={classes.iconColor} />
+          </span>
+          <span>
+            <HiMail className={classes.iconColor} />
+          </span>
+          <span className={classes.imageContainer}>
+            <img className={classes.profileImg} src={user} alt="User Image" />
+          </span>
+        </div>
       </div>
     </nav>
   );
