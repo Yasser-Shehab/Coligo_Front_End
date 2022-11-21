fb0333df869d9273f3b2cdac87808fea06aa8c1b
Yasser-Shehab <yassershehab9@gmail.com>
2022-11-21 12:54:22 +0200

    Added Folder Structure

---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+import { FaHome, FaRegCalendarAlt, FaBook, FaGraduationCap, FaChartLine } from "react-icons/fa";
+import { HiSpeakerphone } from "react-icons/hi";
+import bannerImg from "../assets/images/study.png";
+
+export default {
+  FaHome,
+  FaRegCalendarAlt,
+  FaBook,
+  FaGraduationCap,
+  FaChartLine,
+  HiSpeakerphone,
+  bannerImg,
+};
