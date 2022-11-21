7fe3be4d317baa11fb163eb983b94d1a4e010f4e
Yasser-Shehab <yassershehab9@gmail.com>
2022-11-21 19:28:05 +0200

    Added Fake Api [ Announcment ]

---
+++ src/service/announcment.js
@@ -0,0 +1,35 @@
+import user from "../assets/images/AquaApple.png";
+export const announces = [
+  {
+    id: "user1",
+    name: "Mr.Ahmed Mostafa",
+    icon: user,
+    specialization: "Math 101",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolores. Ducimus molestias quisquam recusandae ea provident reprehenderit illum aut architecto.",
+  },
+  {
+    id: "user2",
+    name: "School managment",
+    icon: user,
+    specialization: "Management",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolores. Ducimus molestias quisquam recusandae ea provident reprehenderit illum aut architecto.",
+  },
+  {
+    id: "user3",
+    name: "Mrs.Salma Ahmed",
+    icon: user,
+    specialization: "Physics 02",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolores. Ducimus molestias quisquam recusandae ea provident reprehenderit illum aut architecto.",
+  },
+  {
+    id: "user4",
+    name: "Events Manager",
+    icon: user,
+    specialization: "Events",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dolores. Ducimus molestias quisquam recusandae ea provident reprehenderit illum aut architecto.",
+  },
+];
